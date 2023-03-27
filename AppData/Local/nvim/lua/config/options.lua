ebf4106d9f43b6de1d91f3ecce04d91329426dbf
ca-mantis-shrimp <dargondab9@gmail.com>
2023-03-26 19:58:35 -0500

    refactor to use the template architecture for entire config folder

--- AppData/Local/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.autochdir = true
-
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
