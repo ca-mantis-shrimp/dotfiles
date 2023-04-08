4a9e1621f273518cf85f4e82b970ceb4653d4614
Darrion Burgess <dargondab9@gmail.com>
2023-04-07 23:05:16 -0500

    add tangled lazy.lua notes!

---
+++ literate_config/Neovim/config/lazy.lua
@@ -0,0 +1,22 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+local spec_list = {
+  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+  { import = "plugins" },
+}
+require("lazy").setup({
+  spec = spec_list,
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true },
+  performance = performance_settings,
+})
+
