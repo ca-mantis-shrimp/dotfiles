be7d40d22048b6ff63022ed51fac2e2708634ac0
Darrion Burgess <dargondab9@gmail.com>
2023-04-09 21:20:41 -0500

    add chatgpt lazygit literate configuration

--- .chezmoitemplates/Neovim/lua/plugins/chat_gpt.lua
+++ .chezmoitemplates/Neovim/lua/plugins/chat_gpt.lua
@@ -1,10 +1,1 @@
-return {
-  "jackMort/ChatGPT.nvim",
-  config = true,
-  cmd = { "ChatGpt" },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
-}
+../../../../literate_config/Neovim/plugins/chatGPT.lua
---
+++ literate_config/Neovim/plugins/chatGPT.lua
@@ -0,0 +1,10 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  config = true,
+  cmd = { "ChatGpt" },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
