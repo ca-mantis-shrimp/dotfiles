1ebdab99a871e87defc170749f8aacf3bc8051de
Darrion Burgess <dargondab9@gmail.com>
2023-04-18 21:20:37 -0500

    add ssh workaround for WSL

--- literate_config/Neovim/plugins/chatGPT.lua
+++ literate_config/Neovim/plugins/chatGPT.lua
@@ -1,10 +1,12 @@
 return {
-	"jackMort/ChatGPT.nvim",
-	config = true,
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
+  "jackMort/ChatGPT.nvim",
+  config = function() 
+    require('chatgpt').setup()
+  end,
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
 }
