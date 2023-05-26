1bd920b030c685375cd7e7142312bb0f6954c9c2
Darrion Burgess <dargondab9@gmail.com>
2023-05-26 18:29:22 -0500

    refactor to use lazy's implementation of neotest

--- literate_config/Neovim/config/lazy.lua
+++ literate_config/Neovim/config/lazy.lua
@@ -13,6 +13,7 @@ local spec_list = {
   {{- end }}
   { import = "lazyvim.plugins.extras.util.project" },
   { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  {import = "lazyvim.plugins.extras.test.core"},
   { import = "lazyvim.plugins.extras.vscode" },
   { import = "plugins" },
 }
