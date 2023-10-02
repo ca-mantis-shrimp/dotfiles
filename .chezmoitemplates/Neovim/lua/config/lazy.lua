3164e4f583b6469ff4597643f18672c7e1e686fe
Darrion Burgess <darburgess@deloitte.com>
2023-10-02 14:58:22 -0700

    add formatting plugins to lazy config

--- .chezmoitemplates/Neovim/lua/config/lazy.lua
+++ .chezmoitemplates/Neovim/lua/config/lazy.lua
@@ -8,18 +8,20 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 
 local spec_list = {
-  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-  {{- if ne .chezmoi.hostname "USMINDARBURGES1" }}
-  { import = "lazyvim.plugins.extras.coding.copilot" },
+  {"LazyVim/LazyVim", import = "lazyvim.plugins"},
+  {{- if ne .chezmoi.hostname "USMINDARBURGES1"}}
+  { import = "lazyvim.plugins.extras.coding.copilot"},
   {{- end }}
-  { import = "lazyvim.plugins.extras.ui.mini-animate" },
-  { import = "lazyvim.plugins.extras.editor.mini-files" },
-  { import = "lazyvim.plugins.extras.ui.edgy" },
-  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+  {import = "lazyvim.plugins.extras.ui.mini-animate"},
+  {import = "lazyvim.plugins.extras.editor.mini-files"},
+  {import = "lazyvim.plugins.extras.ui.edgy"},
+  {import = "lazyvim.plugins.extras.util.mini-hipatterns"},
   {import = "lazyvim.plugins.extras.util.project"},
   {import = "lazyvim.plugins.extras.dap.core"},
+  {import = "lazyvim.plugins.extras.formatting.conform"},
+  {import = "lazyvim.plugins.extras.linting.nvim-lint"},
   {import = "lazyvim.plugins.extras.test.core"},
-  { import = "lazyvim.plugins.extras.vscode" },
+  {import = "lazyvim.plugins.extras.vscode"},
   {import = "lazyvim.plugins.extras.coding.yanky"},
   {import = "lazyvim.plugins.extras.lang.rust"},
   {import = "lazyvim.plugins.extras.lang.python"},
