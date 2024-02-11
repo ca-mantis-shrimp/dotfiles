49a43186296ce617bd086580bdf5251396515616
Darrion Burgess <dargondab9@gmail.com>
2024-02-10 21:01:40 -0800

    added lots of interesting changes for local dev

--- .chezmoitemplates/Neovim/lua/plugins/plugins.lua
+++ .chezmoitemplates/Neovim/lua/plugins/plugins.lua
@@ -37,13 +37,19 @@ return {
 {
   "jackMort/ChatGPT.nvim",
   config = function() 
-    require('chatgpt').setup()
+    require('chatgpt').setup({
+      api_key_cmd = "op read op://Back-End/openai_api_key/credential",
+      openai_params = {
+        model = "gpt-4.0"
+      }
+    })
   end,
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
+    "folke/trouble.nvim"
   },
 },
 
