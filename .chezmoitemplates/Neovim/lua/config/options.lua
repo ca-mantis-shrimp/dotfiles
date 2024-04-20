20767e51a810bef6a306b545c13fc82bd9c7b293
Darrion Burgess <dargondab9@gmail.com>
2024-04-19 21:59:33 -0700

    flirt with importing our own config files

--- .chezmoitemplates/Neovim/lua/config/options.lua
+++ .chezmoitemplates/Neovim/lua/config/options.lua
@@ -1,7 +1,40 @@
-vim.opt.autochdir = true
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = false
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+vim.opt.hlsearch = true
 
 vim.keymap.set('n', '<leader><tab>c', '<cmd>tcd ~/.local/share/chezmoi<CR>', {})
 vim.keymap.set('n', '<leader><tab>r', '<cmd>tcd -<CR>', {})
