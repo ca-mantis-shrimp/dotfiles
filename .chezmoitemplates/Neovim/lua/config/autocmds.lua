6d975348e28abebd0b8691bf1fdafdc91c47ad21
Darrion Burgess <dargondab9@gmail.com>
2024-04-20 22:26:33 -0700

    trying again by just making the changes directly in the source files for now fingers crossed this leads to faster velocity

--- .chezmoitemplates/Neovim/lua/config/autocmds.lua
+++ .chezmoitemplates/Neovim/lua/config/autocmds.lua
@@ -1,8 +1,16 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "norg" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-    vim.opt.spelllang = { "en_us" }
-  end,
+	pattern = { "norg" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+		vim.opt.spelllang = { "en_us" }
+	end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
--- .chezmoitemplates/Neovim/lua/config/options.lua
+++ .chezmoitemplates/Neovim/lua/config/options.lua
@@ -1,33 +1,45 @@
-vim.opt.autochdir = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.opt.showmode = false
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.inccommand = "split"
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+vim.opt.hlsearch = true
 
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
-
-vim.keymap.set('n', '<leader><tab>c', '<cmd>tcd ~/.local/share/chezmoi<CR>', {})
-vim.keymap.set('n', '<leader><tab>r', '<cmd>tcd -<CR>', {})
 
 vim.o.guifont = "FiraCode NF:h20"
 
 vim.g.neovide_scale_factor = 1.0
 
-local change_scale_factor = function(delta)
-  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-end
-
-vim.keymap.set("n", "<C-=>", function()
-  change_scale_factor(1.25)
-end)
-vim.keymap.set("n", "<C-->", function()
-  change_scale_factor(1/1.25)
-end)
-vim.keymap.set("n", "<C-[>", function()
-  vim.g.neovide_scale_factor = 1.0
-end)
-
-if vim.fn.has("windows") then
-  vim.g.python_host_prog = vim.fn.expand('$HOME/python_virtual_environments/dev3.12.0/Scripts/python')
-  vim.g.python3_host_prog = vim.fn.expand('$HOME/python_virtual_environments/dev3.12.0/Scripts/python')
-else
-  vim.g.python_host_prog = vim.fn.expand('$HOME/.pyenv/versions/3.12.0/envs/dev3.12.0/bin/python')
-  vim.g.python3_host_prog = vim.fn.expand('$HOME/.pyenv/versions/3.12.0/envs/dev3.12.0/bin/python')
-end
