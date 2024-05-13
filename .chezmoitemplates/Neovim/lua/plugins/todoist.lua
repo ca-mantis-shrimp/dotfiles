3b91a3f2853f27a8985c681c119baa13c9707f83
Darrion Burgess <dargondab9@gmail.com>
2024-05-12 21:37:55 -0500

    rework to make it easier to actually reload things with the treesitter

--- .chezmoitemplates/Neovim/lua/plugins/todoist.lua
+++ .chezmoitemplates/Neovim/lua/plugins/todoist.lua
@@ -2,22 +2,6 @@ return {
 	"ca-mantis-shrimp/Todoist.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{
-			"ca-manti-shrimp/treesitter-projects",
-			dir = "~/Products/tree-sitter-projects/",
-			dependencies = { "nvim-treesitter/nvim-treesitter" },
-			build = function()
-				local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-				parser_config.projects = {
-					install_info = {
-						url = "~/Products/tree-sitter-projects/",
-						files = { "src/parser.c" },
-					},
-					filetype = "projects",
-				}
-			end,
-		},
 	},
 	dir = "~/Products/Todoist.nvim",
 	config = { api_key = vim.env.TODOIST_API_KEY },
--- .chezmoitemplates/Neovim/lua/plugins/treesitter.lua
+++ .chezmoitemplates/Neovim/lua/plugins/treesitter.lua
@@ -18,13 +18,26 @@ return {
 			},
 		},
 	},
+	dependencies = {
+		"ca-manti-shrimp/treesitter-projects",
+		dir = "~/Products/tree-sitter-projects/",
+	},
 
 	config = function(_, opts)
 		require("nvim-treesitter.install").prefer_git = true
 		if vim.fn.has("win32") == 1 then
 			require("nvim-treesitter.install").compilers = { "zig" }
 		end
-		---@diagnostic disable-next-line: missing-fields
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+		parser_config.projects = {
+			install_info = {
+				url = "~/Products/tree-sitter-projects/",
+				files = { "src/parser.c" },
+			},
+			filetype = "projects",
+		}
+		---@diagnostic disable-next-line: missing-parameter
 		require("nvim-treesitter.configs").setup(opts)
 		vim.opt.foldmethod = "expr"
 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
