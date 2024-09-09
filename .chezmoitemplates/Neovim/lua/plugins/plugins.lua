bdca57f56137b27495af57ddf7e1087676016d6f
Darrion Burgess <dargondab9@gmail.com>
2024-09-08 21:17:00 -0700

    overview to get repl working without hotkey overlap

--- .chezmoitemplates/Neovim/lua/plugins/plugins.lua
+++ .chezmoitemplates/Neovim/lua/plugins/plugins.lua
@@ -57,31 +57,15 @@ return {
 							command = { "python3" }, -- or { "ipython", "--no-autoindent" }
 							format = require("iron.fts.common").bracketed_paste_python,
 						},
+						lua = {
+							command = { "lua" },
+						},
+						["lua.chezmoitmpl"] = { command = { "lua" } },
 					},
 					-- How the repl window will be displayed
 					-- See below for more information
 					repl_open_cmd = require("iron.view").bottom(40),
 				},
-				-- Iron doesn't set keymaps by default anymore.
-				-- You can set them here or manually add keymaps to the functions in iron.core
-				keymaps = {
-					send_motion = "<space>sc",
-					visual_send = "<space>sc",
-					send_file = "<space>sf",
-					send_line = "<space>sl",
-					send_paragraph = "<space>sp",
-					send_until_cursor = "<space>su",
-					send_mark = "<space>sm",
-					mark_motion = "<space>mc",
-					mark_visual = "<space>mc",
-					remove_mark = "<space>md",
-					cr = "<space>s<cr>",
-					interrupt = "<space>s<space>",
-					exit = "<space>sq",
-					clear = "<space>cl",
-				},
-				-- If the highlight is on, you can change how it looks
-				-- For the available options, check nvim_set_hl
 				highlight = {
 					italic = true,
 				},
