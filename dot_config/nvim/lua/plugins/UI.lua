dc5f8e00e5e0508514dd0184cfa46f7b12322953
Darrion Burgess <dargondab9@gmail.com>
2025-02-11 16:08:52 -0800

    feat: add back dadbod

--- dot_config/nvim/lua/plugins/UI.lua
+++ dot_config/nvim/lua/plugins/UI.lua
@@ -1,27 +1,8 @@
 -- [nfnl] Compiled from lua/plugins/UI.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
-	return require("which-key").show({ global = false })
+  return require("which-key").show({global = false})
 end
 local function _2_()
-	return require("dashboard").setup({})
+  return require("dashboard").setup({})
 end
-return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		keys = { { "<leader>?", _1_, desc = "Buffer local keymaps (which-key)" } },
-	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {},
-		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-	},
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opt = { sign = false },
-	},
-	{ "nvimdev/dashboard-nvim", event = "VimEnter", config = _2_, dependencies = { "nvim-tree/nvim-web-devicons" } },
-}
+return {{"folke/which-key.nvim", event = "VeryLazy", keys = {{"<leader>?", _1_, desc = "Buffer local keymaps (which-key)"}}}, {"folke/noice.nvim", event = "VeryLazy", opts = {}, dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"}}, {"folke/todo-comments.nvim", event = "VimEnter", dependencies = {"nvim-lua/plenary.nvim"}, opt = {sign = false}}, {"nvimdev/dashboard-nvim", event = "VimEnter", config = _2_, dependencies = {"nvim-tree/nvim-web-devicons"}}}
