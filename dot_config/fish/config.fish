df2b39d4cf2527ec341d0a71923020521cefe8ea
Darrion Burgess <dargondab9@gmail.com>
2025-03-01 21:22:53 -0800

    adding some more plugins and updates

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,3 +1,7 @@
+# Assumes $OP_SERVICE_ACCOUNT_TOKEN is set prior
+set -gx ANTHROPIC_API_KEY $(op read op://Back-End/anthropic_api_key/credential)
+set -gx TODOIST_API_KEY $(op read op://Back-End/todoist_api_key/credential)
+
 fish_vi_key_bindings
 
 starship init fish | source
