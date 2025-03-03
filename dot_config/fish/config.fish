36857973c3751a2a75dcbd4fe6d0944f3d8e3597
Darrion Burgess <dargondab9@gmail.com>
2025-03-03 10:05:29 -0800

    hopefully i finally have something both ergonomic and secure
    the key is assuming we have a .env file that will contained the needed
    service token without actually putting it in our repo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,14 @@
+# Assumes that the local .env file holds OP service account token
+export (envsubst < ~/.env)
 # Assumes $OP_SERVICE_ACCOUNT_TOKEN is set prior
 set -gx ANTHROPIC_API_KEY $(op read op://Back-End/anthropic_api_key/credential)
 set -gx TODOIST_API_KEY $(op read op://Back-End/todoist_api_key/credential)
+
+switch (cat /etc/hostname) 
+case USSFODARBURGES3
+	set -gx SNOW_MIRROR_DADBOD = $(op read op://work/SNOWMIRROR/dadbod)
+end
+	
 
 fish_vi_key_bindings
 
