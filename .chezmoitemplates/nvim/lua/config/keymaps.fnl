73d52a71384cc5be570066255493a9cc13f669ac
Darrion Burgess <dargondab9@gmail.com>
2025-03-15 21:45:59 -0700

    working through initial pieces that were needed to refactor

--- .chezmoitemplates/nvim/lua/config/keymaps.fnl
+++ .chezmoitemplates/nvim/lua/config/keymaps.fnl
@@ -1,41 +1,52 @@
 (let [wk (require :which-key)]
   (wk.add [{1 :<leader>c :group "[C]ode"}
+           {1 :<leader>u :group "[u]i"}
+           {1 :<leader>ut
+            2 #((. (. (require :neotest) :summary) :toggle))
+            :desc "[u]i to [t]oggle neotest [s]ummary"}
+           {1 :<leader>uc
+            2 "<cmd>CodeCompanionChat Toggle<CR>"
+            :desc "[u]i to toggle code completion [c]hat"}
            {1 :<leader>d :group "[D]ocument"}
-           {1 :<leader>h :group "Git [H]unk"}
-           {1 :<leader>r :group "[R]ename"}
-           {1 :<leader>s :group "[S]earch"}
-           {1 :<leader>t :group "[T]oggle"}
+           {1 :<leader>r :group "[r]ename"}
+           {1 :<leader>s :group "[s]earch"}
+           {1 :<leader>sA
+            2 :<cmd>CodeCompanionActions<CR>
+            :desc "[s]earch CodeCompanion [a]ctions"}
+           {1 :<leader>se
+            2 "<cmd>Telescope neorg search_headings<CR>"
+            :desc "Search Headings"}
+           {1 :<leader>t :group "[t]est"}
+           {1 :<space>tn
+            2 #((. (. (require :neotest) :run) :run))
+            :desc "[t]est the [n]earest case"}
+           {1 :<space>tf
+            2 #((. (. (require :neotest) :run) :run) (_G.vim.fn.expand "%"))
+            :desc "[t]est the current [f]ile"}
+           {1 :<space>ts
+            2 #((. (. (require :neotest) :run) :run) {:suite true})
+            :desc "[t]est the [s]uite"}
            {1 :<leader>w :group "[W]orkspace"}
            {1 :<leader>n :group "[N]eorg"}
-           {1 :<leader>l :group "[L]ists in Neorg"}
+           {1 :<leader>nr
+            2 "<cmd>Neorg return<CR>"
+            :desc "Return from Neorg Workspace"}
+           {1 :<leader>nT
+            2 "<cmd>Neorg tangle current-file<CR>"
+            :desc "Tangle Current File"}
+           {1 :<leader>ni
+            2 "<cmd>Neorg index<CR>"
+            :desc "Go to Index of Neorg Workspace"}
+           {1 :<leader>un
+            2 :<cmd>NoiceDismiss<cr>
+            :desc "Dismiss All Notifications"}
            {1 :<leader>i :group "[I]ntentions"}
            {1 :<leader>ie
             2 #(_G.vim.cmd.edit {:args [(.. (_G.vim.fn.fnamemodify (_G.vim.fn.stdpath :data)
                                                                    ":h")
                                             :/clhd/main.actions)]})
             :desc "Edit clearhead actions file"}
-           {1 :<leader>h :desc "Git [H]unk" :mode :v}
-           {1 :<Esc>
-            2 :<cmd>nohlsearch<CR>
-            :desc "Remove Search Highlighting on Escape"}
-           ;; Diagnositcs
-           {1 "[d"
-            2 #(_G.vim.diagnostic.goto_prev)
-            :desc "Go to previous [D]iagnostic message"}
-           {1 "]d"
-            2 #(_G.vim.diagnostic.goto_next)
-            :desc "Go to next [D]iagnostic message"}
-           {1 :<leader>e
-            2 #(_G.vim.diagnostic.open_float)
-            :desc "Show diagnostic [E]rror messages"}
-           {1 :<leader>q
-            2 #(_G.vim.diagnostic.setloclist)
-            :desc "Open diagnostic [Q]uickfix list"}
-           {1 :<Esc><Esc>
-            2 "<C-\\><C-n>"
-            :mode :t
-            :desc "[Esc]ape terminal mode"}
-           ;; Tabs
+           {1 :<leader><tab> :group "[tab]s"}
            {1 :<leader><tab><tab> 2 :<cmd>tabnew<CR> :desc "Create New [tab]"}
            {1 :<leader><tab><Del>
             2 :<cmd>tabclose<CR>
@@ -48,44 +59,28 @@
            {1 :<leader><tab>b
             2 "<cmd>tcd %:p:h<CR>"
             :desc "CD to [B]uffer Directory"}
+           {1 :<leader>h :desc "Git [H]unk" :mode :v}
+           {1 :<Esc>
+            2 :<cmd>nohlsearch<CR>
+            :desc "Remove Search Highlighting on Escape"}
+           ;; Diagnositcs
+           {1 "[d"
+            2 #(_G.vim.diagnostic.goto_prev)
+            :desc "Go to previous [D]iagnostic message"}
+           {1 "]d"
+            2 #(_G.vim.diagnostic.goto_next)
+            :desc "Go to next [D]iagnostic message"}
+           {1 :<leader>q
+            2 #(_G.vim.diagnostic.setloclist)
+            :desc "Open diagnostic [Q]uickfix list"}
+           {1 :<Esc><Esc>
+            2 "<C-\\><C-n>"
+            :mode :t
+            :desc "[Esc]ape terminal mode"}
            ;; Lazy/Noice
            {1 :<leader>l 2 :<cmd>Lazy<CR> :desc "Start Lazy"}
-           {1 :<leader>un
-            2 :<cmd>NoiceDismiss<cr>
-            :desc "Dismiss All Notifications"}
-           ;; Norg Keymaps
-           {1 :<leader>se
-            2 "<cmd>Telescope neorg search_headings<CR>"
-            :desc "Search Headings"}
-           {1 :<leader>nr
-            2 "<cmd>Neorg return<CR>"
-            :desc "Return from Neorg Workspace"}
-           {1 :<leader>nT
-            2 "<cmd>Neorg tangle current-file<CR>"
-            :desc "Tangle Current File"}
-           {1 :<leader>ni
-            2 "<cmd>Neorg index<CR>"
-            :desc "Go to Index of Neorg Workspace"}
-           ;; Tests
-           {1 :<space>tn
-            2 #((. (. (require :neotest) :run) :run))
-            :desc "[t]est the [n]earest case"}
-           {1 :<space>tf
-            2 #((. (. (require :neotest) :run) :run) (_G.vim.fn.expand "%"))
-            :desc "[t]est the current [f]ile"}
-           {1 :<space>ts
-            2 #((. (. (require :neotest) :run) :run) {:suite true})
-            :desc "[t]est the [s]uite"}
-           {1 :<leader>ut
-            2 #((. (. (require :neotest) :summary) :toggle))
-            :desc "[u]i to [t]oggle neotest [s]ummary"}
            ;; Code Companion
-           {1 :<leader>uc
-            2 "<cmd>CodeCompanionChat Toggle<CR>"
-            :desc "[u]i to toggle code completion [c]hat"}
-           {1 :<leader>sA
-            2 :<cmd>CodeCompanionActions<CR>
-            :desc "[s]earch CodeCompanion [a]ctions"}]))
+           ]))
 
 ;; Window Focus
 (_G.vim.keymap.set :n :<C-h> :<C-w><C-h>
--- .chezmoitemplates/nvim/lua/config/keymaps.lua
+++ .chezmoitemplates/nvim/lua/config/keymaps.lua
@@ -1,93 +1,51 @@
 -- [nfnl] Compiled from lua/config/keymaps.fnl by https://github.com/Olical/nfnl, do not edit.
 do
-	local wk = require("which-key")
-	local function _1_()
-		return _G.vim.cmd.edit({
-			args = { (_G.vim.fn.fnamemodify(_G.vim.fn.stdpath("data"), ":h") .. "/clhd/main.actions") },
-		})
-	end
-	local function _2_()
-		return _G.vim.diagnostic.goto_prev()
-	end
-	local function _3_()
-		return _G.vim.diagnostic.goto_next()
-	end
-	local function _4_()
-		return _G.vim.diagnostic.open_float()
-	end
-	local function _5_()
-		return _G.vim.diagnostic.setloclist()
-	end
-	local function _6_()
-		return require("neotest").run.run()
-	end
-	local function _7_()
-		return require("neotest").run.run(_G.vim.fn.expand("%"))
-	end
-	local function _8_()
-		return require("neotest").run.run({ suite = true })
-	end
-	local function _9_()
-		return require("neotest").summary.toggle()
-	end
-	wk.add({
-		{ "<leader>c", group = "[C]ode" },
-		{ "<leader>d", group = "[D]ocument" },
-		{ "<leader>h", group = "Git [H]unk" },
-		{ "<leader>r", group = "[R]ename" },
-		{ "<leader>s", group = "[S]earch" },
-		{ "<leader>t", group = "[T]oggle" },
-		{ "<leader>w", group = "[W]orkspace" },
-		{ "<leader>n", group = "[N]eorg" },
-		{ "<leader>l", group = "[L]ists in Neorg" },
-		{ "<leader>i", group = "[I]ntentions" },
-		{ "<leader>ie", _1_, desc = "Edit clearhead actions file" },
-		{ "<leader>h", desc = "Git [H]unk", mode = "v" },
-		{ "<Esc>", "<cmd>nohlsearch<CR>", desc = "Remove Search Highlighting on Escape" },
-		{ "[d", _2_, desc = "Go to previous [D]iagnostic message" },
-		{ "]d", _3_, desc = "Go to next [D]iagnostic message" },
-		{ "<leader>e", _4_, desc = "Show diagnostic [E]rror messages" },
-		{ "<leader>q", _5_, desc = "Open diagnostic [Q]uickfix list" },
-		{ "<Esc><Esc>", "<C-\\><C-n>", mode = "t", desc = "[Esc]ape terminal mode" },
-		{ "<leader><tab><tab>", "<cmd>tabnew<CR>", desc = "Create New [tab]" },
-		{ "<leader><tab><Del>", "<cmd>tabclose<CR>", desc = "[Del]ete Current Tab" },
-		{ "<leader><tab>n", "<cmd>tabNext<CR>", desc = "[n]ext Tab" },
-		{ "<leader><tab>p", "<cmd>tabprevious<CR>", desc = "[p]revious Tab" },
-		{ "<leader><tab>r", "<cmd>tcd -<CR>", desc = "CD to previous directory in current tab" },
-		{ "<leader><tab>b", "<cmd>tcd %:p:h<CR>", desc = "CD to [B]uffer Directory" },
-		{ "<leader>l", "<cmd>Lazy<CR>", desc = "Start Lazy" },
-		{ "<leader>un", "<cmd>NoiceDismiss<cr>", desc = "Dismiss All Notifications" },
-		{ "<leader>se", "<cmd>Telescope neorg search_headings<CR>", desc = "Search Headings" },
-		{ "<leader>nr", "<cmd>Neorg return<CR>", desc = "Return from Neorg Workspace" },
-		{ "<leader>nT", "<cmd>Neorg tangle current-file<CR>", desc = "Tangle Current File" },
-		{ "<leader>ni", "<cmd>Neorg index<CR>", desc = "Go to Index of Neorg Workspace" },
-		{ "<space>tn", _6_, desc = "[t]est the [n]earest case" },
-		{ "<space>tf", _7_, desc = "[t]est the current [f]ile" },
-		{ "<space>ts", _8_, desc = "[t]est the [s]uite" },
-		{ "<leader>ut", _9_, desc = "[u]i to [t]oggle neotest [s]ummary" },
-		{ "<leader>uc", "<cmd>CodeCompanionChat Toggle<CR>", desc = "[u]i to toggle code completion [c]hat" },
-		{ "<leader>sA", "<cmd>CodeCompanionActions<CR>", desc = "[s]earch CodeCompanion [a]ctions" },
-	})
+  local wk = require("which-key")
+  local function _1_()
+    return require("neotest").summary.toggle()
+  end
+  local function _2_()
+    return require("neotest").run.run()
+  end
+  local function _3_()
+    return require("neotest").run.run(_G.vim.fn.expand("%"))
+  end
+  local function _4_()
+    return require("neotest").run.run({suite = true})
+  end
+  local function _5_()
+    return _G.vim.cmd.edit({args = {(_G.vim.fn.fnamemodify(_G.vim.fn.stdpath("data"), ":h") .. "/clhd/main.actions")}})
+  end
+  local function _6_()
+    return _G.vim.diagnostic.goto_prev()
+  end
+  local function _7_()
+    return _G.vim.diagnostic.goto_next()
+  end
+  local function _8_()
+    return _G.vim.diagnostic.setloclist()
+  end
+  wk.add({{"<leader>c", group = "[C]ode"}, {"<leader>u", group = "[u]i"}, {"<leader>ut", _1_, desc = "[u]i to [t]oggle neotest [s]ummary"}, {"<leader>uc", "<cmd>CodeCompanionChat Toggle<CR>", desc = "[u]i to toggle code completion [c]hat"}, {"<leader>d", group = "[D]ocument"}, {"<leader>r", group = "[r]ename"}, {"<leader>s", group = "[s]earch"}, {"<leader>sA", "<cmd>CodeCompanionActions<CR>", desc = "[s]earch CodeCompanion [a]ctions"}, {"<leader>se", "<cmd>Telescope neorg search_headings<CR>", desc = "Search Headings"}, {"<leader>t", group = "[t]est"}, {"<space>tn", _2_, desc = "[t]est the [n]earest case"}, {"<space>tf", _3_, desc = "[t]est the current [f]ile"}, {"<space>ts", _4_, desc = "[t]est the [s]uite"}, {"<leader>w", group = "[W]orkspace"}, {"<leader>n", group = "[N]eorg"}, {"<leader>nr", "<cmd>Neorg return<CR>", desc = "Return from Neorg Workspace"}, {"<leader>nT", "<cmd>Neorg tangle current-file<CR>", desc = "Tangle Current File"}, {"<leader>ni", "<cmd>Neorg index<CR>", desc = "Go to Index of Neorg Workspace"}, {"<leader>un", "<cmd>NoiceDismiss<cr>", desc = "Dismiss All Notifications"}, {"<leader>i", group = "[I]ntentions"}, {"<leader>ie", _5_, desc = "Edit clearhead actions file"}, {"<leader><tab>", group = "[tab]s"}, {"<leader><tab><tab>", "<cmd>tabnew<CR>", desc = "Create New [tab]"}, {"<leader><tab><Del>", "<cmd>tabclose<CR>", desc = "[Del]ete Current Tab"}, {"<leader><tab>n", "<cmd>tabNext<CR>", desc = "[n]ext Tab"}, {"<leader><tab>p", "<cmd>tabprevious<CR>", desc = "[p]revious Tab"}, {"<leader><tab>r", "<cmd>tcd -<CR>", desc = "CD to previous directory in current tab"}, {"<leader><tab>b", "<cmd>tcd %:p:h<CR>", desc = "CD to [B]uffer Directory"}, {"<leader>h", desc = "Git [H]unk", mode = "v"}, {"<Esc>", "<cmd>nohlsearch<CR>", desc = "Remove Search Highlighting on Escape"}, {"[d", _6_, desc = "Go to previous [D]iagnostic message"}, {"]d", _7_, desc = "Go to next [D]iagnostic message"}, {"<leader>q", _8_, desc = "Open diagnostic [Q]uickfix list"}, {"<Esc><Esc>", "<C-\\><C-n>", mode = "t", desc = "[Esc]ape terminal mode"}, {"<leader>l", "<cmd>Lazy<CR>", desc = "Start Lazy"}})
 end
-_G.vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-_G.vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-_G.vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-_G.vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+_G.vim.keymap.set("n", "<C-h>", "<C-w><C-h>", {desc = "Move focus to the left window"})
+_G.vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {desc = "Move focus to the right window"})
+_G.vim.keymap.set("n", "<C-j>", "<C-w><C-j>", {desc = "Move focus to the lower window"})
+_G.vim.keymap.set("n", "<C-k>", "<C-w><C-k>", {desc = "Move focus to the upper window"})
 local function change_scale_factor(delta)
-	_G.vim.g.neovide_scale_factor = (_G.vim.g.neovide_scale_factor * delta)
-	return nil
+  _G.vim.g.neovide_scale_factor = (_G.vim.g.neovide_scale_factor * delta)
+  return nil
 end
+local function _9_()
+  return change_scale_factor(1.25)
+end
+_G.vim.keymap.set("n", "<C-=>", _9_)
 local function _10_()
-	return change_scale_factor(1.25)
+  return change_scale_factor((1 / 1.25))
 end
-_G.vim.keymap.set("n", "<C-=>", _10_)
+_G.vim.keymap.set("n", "<C-->", _10_)
 local function _11_()
-	return change_scale_factor((1 / 1.25))
+  _G.vim.g.neovide_scale_factor = 1
+  return nil
 end
-_G.vim.keymap.set("n", "<C-->", _11_)
-local function _12_()
-	_G.vim.g.neovide_scale_factor = 1
-	return nil
-end
-_G.vim.keymap.set("n", "<C-[>", _12_)
-return _G.vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save current Buffer" })
+_G.vim.keymap.set("n", "<C-[>", _11_)
+return _G.vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", {desc = "Save current Buffer"})
