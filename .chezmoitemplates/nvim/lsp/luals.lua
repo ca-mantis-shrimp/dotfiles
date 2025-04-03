8ac7896dce4ff4df3d51453b8a8cd042eb1fae64
Darrion Burgess <dargondab9@gmail.com>
2025-04-03 14:53:25 -0700

    moving the luals name

--- .chezmoitemplates/nvim/lsp/luals.lua
+++ .chezmoitemplates/nvim/lsp/luals.lua
@@ -1,3 +1,2 @@
--- [nfnl] Compiled from lsp/lua.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] Compiled from lsp/luals.fnl by https://github.com/Olical/nfnl, do not edit.
 return {cmd = {"lua-language-server"}, root_marker = {"lua/", "luarc.json"}, filetype = {"lua"}}
-
