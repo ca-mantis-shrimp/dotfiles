ce7ee1de895308c994c06490049f72e6d0c67c30
Darrion Burgess <dargondab9@gmail.com>
2025-07-29 21:13:19 -0700

    working through the bugs with opt_local

--- literate_config/neovim/ftplugin/norg.fnl
+++ literate_config/neovim/ftplugin/norg.fnl
@@ -1,5 +1,6 @@
-(set _G.vim.opt_local.warp true)
-(set _G.opt_local.spell true)
-(set _G.vim._opt_local.spelllang [:en_us])
-(set _G.vim._opt_local.conceallevel 2)
+(set _G.vim.opt_local.wrap true)
+(set _G.vim.opt_local.spell true)
+(set _G.vim.opt_local.spelllang [:en_us])
+(set _G.vim.opt_local.conceallevel 2)
+
 
--- literate_config/neovim/ftplugin/norg.lua
+++ literate_config/neovim/ftplugin/norg.lua
@@ -1,6 +1,6 @@
 -- [nfnl] neovim/ftplugin/norg.fnl
-_G.vim.opt_local.warp = true
-_G.opt_local.spell = true
-_G.vim._opt_local.spelllang = {"en_us"}
-_G.vim._opt_local.conceallevel = 2
+_G.vim.opt_local.wrap = true
+_G.vim.opt_local.spell = true
+_G.vim.opt_local.spelllang = { "en_us" }
+_G.vim.opt_local.conceallevel = 2
 return nil
