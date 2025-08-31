c748fa03634ddd8fd9321483d95aac9297563e7b
Darrion Burgess <dargondab9@gmail.com>
2025-08-30 23:43:28 -0700

    working to make the merge?

--- dot_config/remove_nvim/lua/remove_plugins/formatting.lua
+++ dot_config/remove_nvim/lua/remove_plugins/formatting.lua
@@ -1,1 +1,2 @@
-../../../../literate_config/neovim/plugins/platforms/formatting.lua
+-- [nfnl] lua/remove_plugins/formatting.fnl
+return {"stevearc/conform.nvim", opts = {formatters_by_ft = {lua = {"stylua"}, python = {"ruff", "black"}, fennel = {"fnlfmt"}, clojure = {"cljfmt"}, json = {"jq"}, jsonc = {"jq"}, toml = {"tombi"}, norg = {"injected"}}, format_on_save = {timeout_ms = 500, lsp_format = "fallback"}}}
--- literate_config/neovim/plugins/platforms/formatting.lua
+++ literate_config/neovim/plugins/platforms/formatting.lua
@@ -1,16 +1,2 @@
--- [nfnl] neovim/plugins/platforms/formatting.fnl
-return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff", "black" },
-			fennel = { "fnlfmt" },
-			clojure = { "cljfmt" },
-			json = { "jq" },
-			jsonc = { "jq" },
-			norg = { "injected" },
-		},
-		format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-	},
-}
+-- [nfnl] lua/remove_plugins/formatting.fnl
+return {"stevearc/conform.nvim", opts = {formatters_by_ft = {lua = {"stylua"}, python = {"ruff", "black"}, fennel = {"fnlfmt"}, clojure = {"cljfmt"}, json = {"jq"}, jsonc = {"jq"}, toml = {"tombi"}, norg = {"injected"}}, format_on_save = {timeout_ms = 500, lsp_format = "fallback"}}}
