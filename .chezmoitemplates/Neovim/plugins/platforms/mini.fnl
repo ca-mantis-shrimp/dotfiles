4c6b48194c890b2978ec7312e4afe8d116366e2d
Darrion Burgess <dargondab9@gmail.com>
2025-11-18 20:04:40 -0800

    fixing bug where mini wasnt showing until we used the hotkey

--- .chezmoitemplates/Neovim/plugins/platforms/mini.fnl
+++ .chezmoitemplates/Neovim/plugins/platforms/mini.fnl
@@ -4,13 +4,6 @@
            ((. (require :mini.ai) :setup) {:n_lines 500})
            ((. (require :mini.surround) :setup))
            ((. (require :mini.statusline) :setup))
-           ((. (require :mini.files) :setup))
-           ((. (require :mini.pick) :setup))
            ((. (require :mini.sessions) :setup))
-           ((. (require :mini.tabline) :setup)))
- :keys [[:n
-         :<leader>F
-         (fn []
-           ((. _G.MiniFiles :open)))
-         {:desc "Open File Explorer"}]]}
+           ((. (require :mini.tabline) :setup)))}
 
--- .chezmoitemplates/Neovim/plugins/platforms/mini.lua
+++ .chezmoitemplates/Neovim/plugins/platforms/mini.lua
@@ -3,17 +3,7 @@ local function _1_()
 	require("mini.ai").setup({ n_lines = 500 })
 	require("mini.surround").setup()
 	require("mini.statusline").setup()
-	require("mini.files").setup()
-	require("mini.pick").setup()
 	require("mini.sessions").setup()
 	return require("mini.tabline").setup()
 end
-local function _2_()
-	return _G.MiniFiles.open()
-end
-return {
-	"echasnovski/mini.nvim",
-	version = "*",
-	config = _1_,
-	keys = { { "n", "<leader>F", _2_, { desc = "Open File Explorer" } } },
-}
+return { "echasnovski/mini.nvim", version = "*", config = _1_ }
